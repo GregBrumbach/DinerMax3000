3ee89e078e0af4d7b33f0830ff6728e3319412b5
GregBrumbach <Greg.Brumbach@gmail.com>
2016-09-06 13:27:14 -0400

    WPF Client lesson almost complete

---
+++ DinerMax3000BusinessLayer/Menu.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DinerMax3000.Business.dsDinerMax3000TableAdapters;
+namespace DinerMax3000.Business
+{
+    public class Menu
+    {
+       // constructor should have same name as class and no return item
+        public Menu() 
+        {
+            items = new List<MenuItem>();
+        }
+
+        private int _databaseID;
+
+        public void SaveNewMenuItem(string Name, string Description, double Price)
+        {
+            MenuItemTableAdapter taMenuItem = new MenuItemTableAdapter();
+            taMenuItem.InsertNewMenuItem(Name, Description, Price, _databaseID);
+
+        }
+
+        public static List<Menu> GetAllMenus()
+        {
+            MenuTableAdapter taMenu = new MenuTableAdapter();
+            MenuItemTableAdapter taMenuItem = new MenuItemTableAdapter();
+            var dtMenu = taMenu.GetData();
+            List<Menu> allMenus = new List<Menu>();
+            foreach (dsDinerMax3000.MenuRow menuRow in dtMenu.Rows)
+            {
+                Menu currentMenu = new Menu();
+                currentMenu.Name = menuRow.Name;
+                currentMenu._databaseID = menuRow.ID;
+                allMenus.Add(currentMenu);
+
+                var dtMenuItems = taMenuItem.GetMenuItemsByMenuID(menuRow.ID);
+                foreach (dsDinerMax3000.MenuItemRow menuItemRow in dtMenuItems.Rows)
+                {
+                    currentMenu.AddMenuItem(menuItemRow.Name, menuItemRow.Description, menuItemRow.Price);
+                }
+            }
+
+            return allMenus;
+        }
+
+        public void AddMenuItem(string Title, string Description, double Price)
+        {
+            MenuItem item = new MenuItem();
+            item.Title = Title;
+            item.Description = Description;
+            item.Price = Price;
+            items.Add(item);
+        }
+
+        public string Name { get; set; }
+        public List<MenuItem> items { get; set; } //Menu will have list of members from MenuItem.cs
+    }
+}
---
+++ DinerMax3000Console/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DinerMax3000.Business;
+
+namespace DinerMax3000Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Want to load from DB
+            //FoodMenu summerMenu = new FoodMenu();
+            //summerMenu.Name = "Summer Menu";
+            //summerMenu.AddMenuItem("Salmon","Fresh Norwegian Salmon with Sandefjord butter.",25.50);
+            //summerMenu.AddMenuItem("Taco", "All Norwegians eat Tacos on Friday", 10.00);
+            //summerMenu.HospitalDirections = "Right around corner. Ask for Dr. Strange.";
+
+            //DrinkMenu outsideDrinks = new DrinkMenu();
+            //outsideDrinks.Disclaimer = "Do not drink and code.";
+            //outsideDrinks.AddMenuItem("Virgin Cuba Libre", "A classic!", 10);
+            //outsideDrinks.AddMenuItem("Screwdriver", "Leaves you hammered!", 15);
+
+            List<Menu> menusFromDatabase = Menu.GetAllMenus();
+
+            //Add another item to the first menu
+            Menu firstMenu = menusFromDatabase[0];
+            firstMenu.SaveNewMenuItem("Grits", "A southern classic", 5.0);
+
+            //Reload menu
+            menusFromDatabase = Menu.GetAllMenus();
+
+            Order hungryGuestOrder = new Order();
+            // Want to use DB
+            //for (int x = 0; x <= summerMenu.items.Count - 1; x++)
+            //{
+            //    MenuItem currentItem = summerMenu.items[x];
+            //    hungryGuestOrder.items.Add(currentItem);
+            //}
+
+            foreach (Menu currentMenu in menusFromDatabase)
+            {
+                foreach (MenuItem currentItem in currentMenu.items)
+                {
+                    hungryGuestOrder.items.Add(currentItem);
+                }
+            }
+            Console.WriteLine("The total is: " + hungryGuestOrder.Total);
+
+            // Used prior to DB
+            //try
+            //{
+
+            //    outsideDrinks.AddMenuItem("Himkok", "9 of 10 people recommend avoiding this drink", 0);
+            //}
+            //catch (Exception thrownException)
+            //{
+            //    Console.WriteLine(thrownException.Message);
+            //}
+            Console.ReadKey();
+
+        }
+    }
+}
